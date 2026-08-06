f8b7e5f76a0bfe319b722b408cc164fd1ab38a43
annapauseiro <130695491+annapauseiro@users.noreply.github.com>
2026-08-06 14:19:23 +0100

    Update truncate_insert.sql

--- scripts/truncate_insert.sql
+++ scripts/truncate_insert.sql
@@ -1,3 +1,5 @@
+/* Creating SQL DDL scripts for ALL CSV files in the CRM and ERP sys. */
+
 TRUNCATE TABLE bronze.crm_cust_info;
 BULK INSERT bronze.crm_cust_info
 FROM 'C:\Users\apauseir\OneDrive - Cisco\Documents\Pessoal\Data Engineer\SQL\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
@@ -51,3 +53,24 @@ WITH (
 	FIELDTERMINATOR = ',',
 	TABLOCK
 )
+
+
+/* Quality check: check that the data has not shifted and is in the correct columns and number of rows.*/
+
+SELECT * FROM bronze.crm_cust_info
+SELECT COUNT (*) FROM bronze.crm_cust_info;
+
+SELECT * FROM bronze.crm_prd_info
+SELECT COUNT (*) FROM bronze.crm_prd_info;
+
+SELECT * FROM bronze.crm_sales_details
+SELECT COUNT (*) FROM bronze.crm_sales_details;
+
+SELECT * FROM bronze.erp_cust_az12
+SELECT COUNT (*) FROM bronze.erp_cust_az12;
+
+SELECT * FROM bronze.erp_loc_a101
+SELECT COUNT (*) FROM bronze.erp_loc_a101;
+
+SELECT * FROM bronze.erp_px_cat_g1v2
+SELECT COUNT (*) FROM bronze.erp_px_cat_g1v2
